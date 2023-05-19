3a2ba15485f584c4c6c9cfda6736899a3f32f7f6
DeeRayquza <52299512+DeeRayquza@users.noreply.github.com>
2023-05-19 15:33:07 +0100

    Update perception.m

--- Project/perception.m
+++ Project/perception.m
@@ -2,7 +2,7 @@
 train_speed = 50; % km/h
 observer_distance = 5; % m
 object_distance = 10; % m
-object_speeds = [0, 10, -10]; % m/s
+object_speed = 0; % stationary object
 time_step = 0.1; % seconds
 duration = 10; % seconds
 
@@ -11,7 +11,6 @@ time_array1 = 0;
 perceived_speed_array1 = 0;
 time_array2 = 0;
 perceived_speed_array2 = 0;
-observer_distance_array2 = 0;
 
 % Initialize variables
 observer_position = observer_distance;
@@ -35,42 +34,29 @@ while time < duration
 end
 
 % Simulate motion while observing object
-for object_speed = object_speeds
-    % Reset variables
-    observer_position = observer_distance;
-    object_position = object_distance;
-    time = 0;
+time = 0;
+while time < duration
+    % Update observer and object positions
+    observer_position = observer_position + (train_speed/3.6) * time_step;
+    object_position = object_position + (object_speed/3.6) * time_step;
     
-    while time < duration
-        % Update observer and object positions
-        observer_position = observer_position + (train_speed/3.6) * time_step;
-        object_position = object_position + (object_speed) * time_step;
-        
-        % Calculate perceived speed
-        perceived_speed2 = train_speed * (object_distance + object_position - observer_position) / (object_distance + observer_position);
-        
-        % Update time
-        time = time + time_step;
-        
-        % Store results
-        time_array2(end+1) = time;
-        perceived_speed_array2(end+1) = perceived_speed2;
-        observer_distance_array2(end+1) = observer_position - object_position;
-    end
+    % Calculate perceived speed
+    perceived_speed2 = train_speed * (object_distance + object_position - observer_position) / (object_distance + observer_position);
+    
+    % Update time
+    time = time + time_step;
+    
+    % Store results
+    time_array2(end+1) = time;
+    perceived_speed_array2(end+1) = perceived_speed2;
 end
 
 % Plot results
-figure(1);
+figure;
 plot(time_array1, perceived_speed_array1, 'b-', 'LineWidth', 2);
 hold on;
 plot(time_array2, perceived_speed_array2, 'r-', 'LineWidth', 2);
 xlabel('Time (s)');
 ylabel('Perceived speed (km/h)');
 legend('Without observing object', 'While observing object');
-grid on;
-
-figure(2);
-plot(observer_distance_array2, perceived_speed_array2, 'r-', 'LineWidth', 2);
-xlabel('Distance to observed object (m)');
-ylabel('Perceived speed (km/h)');
 grid on;
